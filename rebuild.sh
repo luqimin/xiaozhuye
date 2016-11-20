35f6c0a6c7c06fe06cda148eb94fd0db084ea524
QiminLU <qilu@163.com>
2016-11-20 23:09:30 +0800

    优化rebuild.sh命令样式

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
+echo '\n\033[35m> 初始化文件... \033[0m'
 git checkout view/home/index_index.html
+echo '> view/home/index_index.html => clean'
+
+echo '\n\033[35m> git pull... \033[0m'
 git pull origin
+
+# echo '\n\033[35m> npm install... \033[0m'
 # npm install
-node ./fe-xiaozhuye.js
+
+echo '\n\033[35m> 打包前端文件... \033[0m'
+npm run fe-xiaozhuye
+
+echo '\n\033[35m> 打包后端文件... \033[0m'
 npm run compile
+
+echo '\n\033[35m> 重启服务pm2... \033[0m'
 pm2 restart xiaozhuye
