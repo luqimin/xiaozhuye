46e4eb0491e615002af06cc458381dc0cdc350a8
QiminLU <qilu@163.com>
2016-11-27 15:24:32 +0800

    修改新闻等接口，使用axios

--- src/addons/controller/fetch.js
+++ src/addons/controller/fetch.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import Base from './base.js';
-import request from "request";
 import axios from 'axios';
 import _ from 'lodash';
 export default class extends Base {
@@ -54,83 +53,35 @@ export default class extends Base {
 
     //新闻头条
     async toutiaoAction() {
-        let getApiData = () => {
-            let fn = think.promisify(request.get);
-            return fn({
-                url: "http://v.juhe.cn/toutiao/index?type=top&key=1cbcc9bbbced658f6c56e7fa695e4fa3"
-            });
-        };
-
-        let msg = getApiData();
-
-        let result = await Promise.all([msg]);
-        let resArr = JSON.parse(result[0].body).result.data;
-        this.end(_.dropRight(resArr, 21));
+        let res = await axios.get('http://v.juhe.cn/toutiao/index?type=top&key=1cbcc9bbbced658f6c56e7fa695e4fa3');
+        let result = res.data.result.data;
+        this.end(_.dropRight(result, 21));
     }
 
     //国内焦点
     async gnfocusAction() {
-        let getApiData = () => {
-            let fn = think.promisify(request.get);
-            return fn({
-                url: "https://route.showapi.com/109-35?channelId=5572a108b3cdc86cf39001cd&maxResult=10&needAllList=0&needContent=0&needHtml=0&page=1&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617"
-            });
-        };
-
-        let msg = getApiData();
-
-        let result = await Promise.all([msg]);
-        let resArr = JSON.parse(result[0].body).showapi_res_body.pagebean.contentlist;
-        this.end(resArr);
+        let res = await axios.get('https://route.showapi.com/109-35?channelId=5572a108b3cdc86cf39001cd&maxResult=10&needAllList=0&needContent=0&needHtml=0&page=1&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617');
+        let result = res.data.showapi_res_body.pagebean.contentlist;
+        this.end(result);
     }
 
     //国外焦点
     async gwfocusAction() {
-
-        let getApiData = () => {
-            let fn = think.promisify(request.get);
-            return fn({
-                url: "https://route.showapi.com/109-35?channelId=5572a108b3cdc86cf39001ce&maxResult=10&needAllList=0&needContent=0&needHtml=0&page=1&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617"
-            });
-        };
-
-        let msg = getApiData();
-
-        let result = await Promise.all([msg]);
-        let resArr = JSON.parse(result[0].body).showapi_res_body.pagebean.contentlist;
-        this.end(resArr);
+        let res = await axios.get('https://route.showapi.com/109-35?channelId=5572a108b3cdc86cf39001ce&maxResult=10&needAllList=0&needContent=0&needHtml=0&page=1&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617');
+        let result = res.data.showapi_res_body.pagebean.contentlist;
+        this.end(result);
     }
 
     //娱乐焦点
     async yuleAction() {
-
-        let getApiData = () => {
-            let fn = think.promisify(request.get);
-            return fn({
-                url: "https://route.showapi.com/109-35?channelId=5572a108b3cdc86cf39001d5&maxResult=10&needAllList=0&needContent=0&needHtml=0&page=1&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617"
-            });
-        };
-
-        let msg = getApiData();
-
-        let result = await Promise.all([msg]);
-        let resArr = JSON.parse(result[0].body).showapi_res_body.pagebean.contentlist;
-        this.end(resArr);
+        let res = await axios.get('https://route.showapi.com/109-35?channelId=5572a10ab3cdc86cf39001eb&maxResult=10&needAllList=0&needContent=0&needHtml=0&page=1&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617');
+        let result = res.data.showapi_res_body.pagebean.contentlist;
+        this.end(result);
     }
 
     async duanziAction() {
-
-        let getApiData = () => {
-            let fn = think.promisify(request.get);
-            return fn({
-                url: "https://route.showapi.com/255-1?page=&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617"
-            });
-        };
-
-        let msg = getApiData();
-
-        let result = await Promise.all([msg]);
-        let resArr = JSON.parse(result[0].body).showapi_res_body.pagebean.contentlist;
-        return this.end(_.dropRight(resArr, 10));
+        let res = await axios.get('https://route.showapi.com/255-1?page=&showapi_appid=25653&showapi_sign=fde151b148b6494aa99d07426967b617');
+        let result = res.data.showapi_res_body.pagebean.contentlist;
+        this.end(result);
     }
 }
