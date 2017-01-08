031107ac7f7b821d7b6ad31975497cd9e877caad
QiminLU <qilu@163.com>
2017-01-08 14:44:54 +0800

    环境部署git地址修改为oschina

--- rebuild.sh
+++ rebuild.sh
@@ -5,7 +5,7 @@ git checkout view/home/index_index.html
 echo -e '> view/home/index_index.html => clean'
 
 echo -e '\n\033[35m> git pull... \033[0m'
-git pull origin
+git pull gitosc master
 
 echo -e '\n\033[35m> npm install... \033[0m'
 npm install
