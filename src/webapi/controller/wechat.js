dd983a6f2ad927758453cf805acd5c33c94059d9
QiminLU <qilu@163.com>
2017-03-19 02:19:32 +0800

    debug

--- src/webapi/controller/wechat.js
+++ src/webapi/controller/wechat.js
@@ -6,6 +6,15 @@ import xml2js from 'xml2js';
 let parser = new xml2js.Parser();
 let builder = new xml2js.Builder();
 
+import crypto from "crypto";
+
+function sha1(str) {
+    var md5sum = crypto.createHash("sha1");
+    md5sum.update(str);
+    str = md5sum.digest("hex");
+    return str;
+}
+
 export default class extends Base {
     /**
      * index action
@@ -17,6 +26,26 @@ export default class extends Base {
     }
 
     async postmsgAction() {
+
+        let signature = this.get('signature');
+        let timestamp = this.get('timestamp');
+        let nonce = this.get('nonce');
+        let echostr = this.get('echostr');
+
+        let oriArray = [];
+        oriArray[0] = nonce;
+        oriArray[1] = timestamp;
+        oriArray[2] = 'xiaozhuye';
+        oriArray.sort();
+        let original = oriArray.join('');
+        let scyptoString = sha1(original);
+
+        if (signature == scyptoString) {
+            this.end(echostr);
+        } else {
+            this.end("false");
+        }
+
         let autoEnd = setTimeout(() => {
             this.end('');
         }, 3000);
